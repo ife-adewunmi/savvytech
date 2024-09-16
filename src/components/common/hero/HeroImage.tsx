fde0760692a0d762a1cad60fa29bf904c034b3ef
Ife-adewunmi <ifeoluwa.adewunmi94@gmail.com>
2024-09-16 21:04:53 +0100

    PS-update-hero-image-component

--- src/components/common/hero/HeroImage.tsx
+++ src/components/common/hero/HeroImage.tsx
@@ -1,34 +1,24 @@
 import React from 'react'
-import Carousel from '@/components/Carousel'
-import { FaArrowRightLong, FaArrowLeftLong } from 'react-icons/fa6'
-import SectionContainer from '@/components/common/section/SectionContainer'
+import {ImageInterface} from "@/data/hero/heroData";
 
-interface HeroCarouselProps {}
+interface HeroImageProps {
+  images: Array<ImageInterface>
+  imagePosition?: string
+  gridCols?: number
+}
 
-const HeroImage = () => {
-  const prevButton = <FaArrowLeftLong className={`text-base lg:text-lg xl:text-2xl`} />
-
-  const nextButton = <FaArrowRightLong className={`text-base lg:text-lg xl:text-2xl`} />
+const HeroImage = ({ images, gridCols }: HeroImageProps) => {
+  const gridCol = gridCols || 2
 
   return (
     <div className="h-full">
-      <div className="grid h-full grid-cols-2 gap-4">
-        <div
-          key={1}
-          className="h-full rounded-2xl bg-about_img_1 bg-contain bg-right  bg-no-repeat "
-        />
-        <div
-          key={2}
-          className="h-full rounded-2xl bg-about_img_2 bg-cover bg-bottom bg-no-repeat "
-        />
-        <div
-          key={3}
-          className="h-full rounded-2xl bg-about_img_3 bg-contain bg-right-top bg-no-repeat "
-        />
-        <div
-          key={4}
-          className="h-full rounded-2xl bg-about_img_4 bg-contain bg-left bg-no-repeat "
-        />
+      <div className={`grid h-full grid-cols-${gridCol} gap-4`}>
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`h-full rounded-2xl ${image.img} bg-contain ${image.pos}  bg-no-repeat`}
+          />
+        ))}
       </div>
     </div>
   )
